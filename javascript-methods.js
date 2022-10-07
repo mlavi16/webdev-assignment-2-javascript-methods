19637724520ae8401815cff9708e37e275aa2786
mlavi16 <Maya.lavi11@gmail.com>
2022-10-07 18:26:39 -0400

    cleaning up testing messages

--- javascript-methods.js
+++ javascript-methods.js
@@ -25,6 +25,7 @@ Array.prototype.myMap = function(callbackFn) {
 Array.prototype.myFilter = function(callbackFn) {
   let filteredArray = [];
   for (let i = 0; i < this.length; i++) {
+    if (this[i] == undefined) continue;
     if (callbackFn(this[i], i, this)) {
       filteredArray.push(this[i]);
     }
@@ -35,6 +36,7 @@ Array.prototype.myFilter = function(callbackFn) {
 // SOME //
 Array.prototype.mySome = function(callbackFn) {
   for (let i = 0; i < this.length; i++) {
+    if (this[i] == undefined) continue;
     if (callbackFn(this[i], i, this)) {
       return true;
     }
@@ -45,6 +47,7 @@ Array.prototype.mySome = function(callbackFn) {
 // EVERY //
 Array.prototype.myEvery = function(callbackFn) {
   for (let i = 0; i < this.length; i++) {
+    if (this[i] == undefined) continue;
     if (!callbackFn(this[i], i, this)) {
       return false;
     }
@@ -116,8 +119,9 @@ Object.myValues = function(object) {
 
 
 
-
+/*
 // -----------------------Testing Functions--------------------------------- //
+
 
 Array.prototype.equals = function (array) {
   // if the other array is a falsy value, return
@@ -146,149 +150,146 @@ Array.prototype.equals = function (array) {
 }
 
 function printEquals(a, b) {
-  console.log(a, " == ", b, " is ", a == b);
+  console.log(a, "\t == \t", b, "\t is \t", a == b);
 }
 
 function printEqualsArray(a, b) {
   console.log(a, " == ", b, " is ", a.equals(b));
 }
 
+
 // Map //
-console.log("----TESTING MAP FUNCTION: myMap()----")
-let mapArray = [0, -10, 3, 7, ,102];
-printEqualsArray(mapArray.map(x => x * 2), mapArray.myMap(x => x * 2))
-mapArray = [1, 4, 9];
-printEqualsArray(mapArray.map((num) => Math.sqrt(num)), mapArray.myMap((num) => Math.sqrt(num)));
-
-// // Filter //
-// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-// const result1 = words.filter(word => word.length > 6);
-// const result2 = words.myFilter(word => word.length > 6);
-// result1.equals(result2); // [ 'exuberant', 'destruction', 'present' ]
-// const filterArray = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-// function isPrime(num) {
-//   for (let i = 2; num > i; i++) {
-//     if (num % i === 0) {
-//       return false;
-//     }
-//   }
-//   return num > 1;
-// }
-// filterArray.filter(isPrime).equals(filterArray.myFilter(isPrime)) // [2, 3, 5, 7, 11, 13]
+console.log("----TESTING MAP FUNCTION: map() == myMap()----")
+let mapTestArray = [0, -10, 3, 7, ,102];
+printEqualsArray(mapTestArray.map(x => x * 2), mapTestArray.myMap(x => x * 2))
+mapTestArray = [1, 4, 9];
+printEqualsArray(mapTestArray.map((num) => Math.sqrt(num)), mapTestArray.myMap((num) => Math.sqrt(num)));
 
 
-// // Some //
-// const even = (element) => element % 2 === 0; // checks whether an element is even
-// let someArray = [];
-// console.log(someArray.some(even) == (someArray.mySome(even)));
-// someArray = [1];
-// console.log(someArray.some(even) == (someArray.mySome(even)));
-// someArray = [2];
-// console.log(someArray.some(even) == (someArray.mySome(even)));
-// someArray = [1,2,3,4,5,6];
-// console.log(someArray.some(even) == (someArray.mySome(even)));
-// someArray = [1,33,17,67];
-// console.log(someArray.some(even) == (someArray.mySome(even)));
-// someArray = [1,-2, -3, , , , -9];
-// console.log(someArray.some(even) == (someArray.mySome(even)));
-// function isBiggerThan10(element, index, array) {
-//   return element > 10;
-// }
-// console.log([2, 5, 8, 1, 4].some(isBiggerThan10) == [2, 5, 8, 1, 4].mySome(isBiggerThan10));  // false
-// console.log([12, 5, 8, 1, 4].some(isBiggerThan10) == [12, 5, 8, 1, 4].mySome(isBiggerThan10)); // true
+// Filter //
+console.log("----TESTING FILTER FUNCTION: filter() == myFilter()----")
+let filterTestArray = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+printEqualsArray(filterTestArray.filter(word => word.length > 6), filterTestArray.myFilter(word => word.length > 6)); // [ 'exuberant', 'destruction', 'present' ]
+filterTestArray = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, , 8, 9, 10, 11, 12, 13];
+function isPrime(num) {
+  for (let i = 2; num > i; i++) {
+    if (num % i === 0)
+      return false;
+  }
+  return num > 1;
+}
+printEqualsArray(filterTestArray.filter(isPrime), (filterTestArray.myFilter(isPrime))); // [2, 3, 5, 7, 11, 13]
 
 
-
-// // Every //
-
-
-
-// // Reduce //
-// const reduceArray = [1, 2, 3, , 4, 0, -5]; // 1 + 2 + 3 + 4 + 0 - 5 = 5
-// const sumWithInitial1 = reduceArray.reduce(
-//   (previousValue, currentValue) => previousValue + currentValue);
-// const sumWithInitial2 = reduceArray.myReduce(
-//   (previousValue, currentValue) => previousValue + currentValue);
-// // console.log(sumWithInitial1, sumWithInitial2, sumWithInitial1 == sumWithInitial2);
-
-
-// // Includes //
-// const includesArray = [1, 2, 3];
-// // console.log(includesArray.includes(2), includesArray.myIncludes(2));
-// // console.log(includesArray.includes(), includesArray.myIncludes());
-// // console.log(includesArray.includes(true), includesArray.myIncludes(true));
-// // console.log(includesArray.includes(-1), includesArray.myIncludes(-1));
-// const pets = ['cat', 'dog', ,'bat'];
-// // console.log(pets.includes('cat'), pets.myIncludes('cat'));
-// // console.log(pets.includes('at'), pets.myIncludes('at'));
+// Some //
+console.log("----TESTING SOME FUNCTION: some() == mySome()----")
+let even = (element) => element % 2 === 0; // checks whether an element is even
+printEquals([].some(even), [].mySome(even));
+printEquals([1].some(even), [1].mySome(even));
+printEquals([2].some(even), [2].mySome(even));
+// printEquals([2,3,4,5,6].some(even), [2,3,4,5,6].mySome(even));
+// printEquals([1,33,17,67].some(even), [1,33,17,67].mySome(even));
+// printEquals([1,-2, -3, , -9].some(even), [1,-2, -3, , -9].mySome(even));
+// printEquals([2, 5, 8, 1, 4, -10].some(element => element > 10) , [2, 5, 8, 1, 4, -10].mySome(element => element > 10)); 
+printEquals([12, 5, 8, 1, 4].some(element => element > 10) , [12, 5, 8, 1, 4].mySome(element => element > 10));
 
 
-// // IndexOf and LastIndexOf//
-// const beasts = ['ant', , 'bison', 'camel', 'duck', 'bison'];
-// // console.log(beasts.indexOf('bison'), beasts.myIndexOf('bison')); // expected output: 2
-// // console.log(beasts.lastIndexOf('bison'), beasts.myLastIndexOf('bison')); // expected output: 5
-// // console.log(beasts.indexOf('giraffe'), beasts.myIndexOf('giraffe')); // expected output: -1
-// // console.log(beasts.lastIndexOf('giraffe'), beasts.myLastIndexOf('giraffe')); // expected output: -1
-// // console.log(beasts.indexOf('duck'), beasts.myIndexOf('duck')); // expected output: 4
-// // console.log(beasts.lastIndexOf('duck'), beasts.myLastIndexOf('duck')); // expected output: 4
-// const indexOfArray = [2, 9, 9];
-// // console.log(indexOfArray.indexOf(2), indexOfArray.myIndexOf(2)); // 0
-// // console.log(indexOfArray.lastIndexOf(2), indexOfArray.myLastIndexOf(2)); // 0
-// // console.log(indexOfArray.indexOf(7), indexOfArray.myIndexOf(7)); // -1
-// // console.log(indexOfArray.lastIndexOf(7), indexOfArray.myLastIndexOf(7)); // -1
-// // console.log(indexOfArray.indexOf(9), indexOfArray.myIndexOf(9)); // 1
-// // console.log(indexOfArray.lastIndexOf(9), indexOfArray.myLastIndexOf(9)); // 2
+// Every //
+console.log("----TESTING EVERY FUNCTION: every() == myEvery()----")
+let everyTestArray = [1, 30, 39, -29, 10, -13];
+printEquals(everyTestArray.every(val => val < 40), everyTestArray.myEvery(val => val < 40));
+printEquals(everyTestArray.every(val => val > 0), everyTestArray.myEvery(val => val > 0));
+// let isSubset = (array1, array2) => array2.every((element) => array1.includes(element));
+// let myIsSubset = (array1, array2) => array2.myEvery((element) => array1.includes(element));
+// printEquals(isSubset([1, 2, 3, 4, 5, 6, 7], [5, 7, 6]), myIsSubset([1, 2, 3, 4, 5, 6, 7], [5, 7, 6])); // true
+// printEquals(isSubset([1, 2, 3, 4, 5, 6, 7], [5, 8, 7]), myIsSubset([1, 2, 3, 4, 5, 6, 7], [5, 8, 7])); // false
+printEquals([1, , 3].every((x) => x !== undefined), [1, , 3].myEvery((x) => x !== undefined)); // true
+printEquals([2, , 2].every((x) => x === 2), [2, , 2].myEvery((x) => x === 2)); // true
 
 
-
-// // Keys //
-
-// // simple array
-// const keysArr = ['a', 'b', 'c'];
-// console.log(Object.keys(keysArr), Object.myKeys(keysArr)); // console: ['0', '1', '2']
-// // array-like object
-// const obj1 = { 0: 'a', 1: 'b', 2: 'c' };
-// const obj2 = {a: 'somestring', b: 42, c: false };
-// console.log(Object.keys(obj1), Object.myKeys(obj1)); // console: ['0', '1', '2']
-// console.log(Object.keys(obj2), Object.myKeys(obj2)); // console: ['a', 'b', 'c']
-// // array-like object with random key ordering
-// const anObj = { 100: 'a', 2: 'b', 7: 'c' };
-// console.log(Object.keys(anObj), Object.myKeys(anObj)); // console: ['2', '7', '100']
-// // getFoo is a property which isn't enumerable
-// const myObj = Object.create({}, {
-//   getFoo: {
-//     value() { return this.foo; }
-//   }
-// });
-// myObj.foo = 1;
-// console.log(Object.keys(myObj), Object.myKeys(myObj)); // console: ['foo']
+// Reduce //
+console.log("----TESTING REDUCE FUNCTION: reduce() == myReduce()----")
+let reduceTestArray = [1, 3, 2, , 4, 0, -5]; // 1 + 3 + 2 + 4 + 0 - 5 = 5
+printEquals(reduceTestArray.reduce((prevVal, curVal) => prevVal + curVal), reduceTestArray.myReduce((prevVal, curVal) => prevVal + curVal));
+printEquals(reduceTestArray.reduce((a, b) => Math.max(a, b)), reduceTestArray.myReduce((a, b) => Math.max(a, b)));
+// let reduceTestObjects = [{ x: 1 }, { x: 2 }, { x: 3 }];
+// printEquals(reduceTestObjects.reduce((previousValue, currentValue) => previousValue + currentValue.x), reduceTestObjects.myReduce((previousValue, currentValue) => previousValue + currentValue.x));
 
 
+// Includes //
+console.log("----TESTING INCLUDES FUNCTION: includes() == myIncludes()----")
+let includesTestArray = [1, 2, 3];
+printEquals(includesTestArray.includes(2), includesTestArray.myIncludes(2));
+// printEquals(includesTestArray.includes(-1), includesTestArray.myIncludes(-1));
+// printEquals(includesTestArray.includes(), includesTestArray.myIncludes());
+printEquals(includesTestArray.includes(true), includesTestArray.myIncludes(true));
+includesTestArray = ['cat', 'dog', ,'bat'];
+printEquals(includesTestArray.includes('cat'), includesTestArray.myIncludes('cat'));
+printEquals(includesTestArray.includes('Cat'), includesTestArray.myIncludes('Cat'));
 
-// // Values //
 
-// // Simple array
-// let valsArr = ['a', 'b', 'c'];
-// console.log(Object.values(valsArr), Object.myValues(valsArr)); // console: ['0', '1', '2']
+// IndexOf and LastIndexOf//
+console.log("----TESTING INDEXOF FUNCTION: indexOf() == myIndexOf()----")
+let indexTestArray1 = ['ant', , 'bison', 'camel', 'duck', 'bison'];
+let indexTestArray2 = [2, 9, 9];
+printEquals(indexTestArray1.indexOf('bison'), indexTestArray1.myIndexOf('bison')); // expected output: 2
+printEquals(indexTestArray1.indexOf('giraffe'), indexTestArray1.myIndexOf('giraffe')); // expected output: -1
+// printEquals(indexTestArray1.indexOf('duck'), indexTestArray1.myIndexOf('duck')); // expected output: 4
+// printEquals(indexTestArray2.indexOf(2), indexTestArray2.myIndexOf(2)); // 0
+// printEquals(indexTestArray2.indexOf(7), indexTestArray2.myIndexOf(7)); // -1
+printEquals(indexTestArray2.indexOf(9), indexTestArray2.myIndexOf(9)); // 1
 
-// // Objects
-// let valObj = { a: 'somestring', b: 42, c: false };
-// console.log(Object.values(valObj), Object.myValues(valObj));// expected output: Array ["somestring", 42, false]
-// valObj = { foo: 'bar', baz: 42 };
-// console.log(Object.values(valObj), Object.myValues(valObj)); // ['bar', 42]
+console.log("----TESTING LASTINDEXOF FUNCTION: lastIndexOf() == myLastIndexOf()----")
+printEquals(indexTestArray1.lastIndexOf('bison'), indexTestArray1.myLastIndexOf('bison')); // expected output: 5
+printEquals(indexTestArray1.lastIndexOf('giraffe'), indexTestArray1.myLastIndexOf('giraffe')); // expected output: -1
+// printEquals(indexTestArray1.lastIndexOf('duck'), indexTestArray1.myLastIndexOf('duck')); // expected output: 4
+// printEquals(indexTestArray2.lastIndexOf(2), indexTestArray2.myLastIndexOf(2)); // 0
+// printEquals(indexTestArray2.lastIndexOf(7), indexTestArray2.myLastIndexOf(7)); // -1
+printEquals(indexTestArray2.lastIndexOf(9), indexTestArray2.myLastIndexOf(9)); // 2
 
-// // Array-like object
-// let valArrayLikeObj = { 0: 'a', 1: 'b', 2: 'c' };
-// console.log(Object.values(valArrayLikeObj), Object.myValues(valArrayLikeObj)); // ['a', 'b', 'c']
 
-// // Array-like object with random key ordering
-// valArrayLikeObj = { 100: 'a', 2: 'b', 7: 'c' };
-// console.log(Object.values(valArrayLikeObj), Object.myValues(valArrayLikeObj)); // ['b', 'c', 'a']
+// Keys //
+console.log("----TESTING KEYS FUNCTION: Object.keys() == Object.myKeys()----")
+// simple array
+let keysTestArr = ['a', 'b', 'c'];
+console.log(Object.keys(keysTestArr), " == ", Object.myKeys(keysTestArr)); // console: ['0', '1', '2']
+// object
+let keysTestobj = {a: 'somestring', b: 42, c: false };
+console.log(Object.keys(keysTestobj), " == ", Object.myKeys(keysTestobj)); // console: ['a', 'b', 'c']
+// array-like object
+keysTestobj = { 0: 'a', 1: 'b', 2: 'c' };
+console.log(Object.keys(keysTestobj), " == ", Object.myKeys(keysTestobj)); // console: ['0', '1', '2']
+// array-like object with random key ordering
+keysTestobj = { 100: 'a', 2: 'b', 7: 'c' };
+console.log(Object.keys(keysTestobj), " == ", Object.myKeys(keysTestobj)); // console: ['2', '7', '100']
+// getFoo is a property which isn't enumerable
+keysTestobj = Object.create({}, { getFoo: { value() { return this.foo; } } });
+keysTestobj.foo = 1;
+console.log(Object.keys(keysTestobj), " == ", Object.myKeys(keysTestobj)); // console: ['foo']
 
-// // getFoo is property which isn't enumerable
-// valObj = Object.create({}, { getFoo: { value() { return this.foo; } } });
-// valObj.foo = 'bar';
-// console.log(Object.values(valObj), Object.myValues(valObj)); // ['bar']
 
-// // non-object argument
-// console.log(Object.values('foo'), Object.myValues('foo')); // ['f', 'o', 'o']
+// Values //
+console.log("----TESTING VALUES FUNCTION: Object.values() == Object.myValues()----")
+// Simple array
+let valuesTestArr = ['a', 'b', 'c'];
+console.log(Object.values(valuesTestArr), " == ", Object.myValues(valuesTestArr)); // console: ['0', '1', '2']
+// Objects
+let valuesTestObj = { a: 'somestring', b: 42, c: false };
+console.log(Object.values(valuesTestObj), " == ", Object.myValues(valuesTestObj));// expected output: Array ["somestring", 42, false]
+valuesTestObj = { foo: 'bar', baz: 42 };
+console.log(Object.values(valuesTestObj), " == ", Object.myValues(valuesTestObj)); // ['bar', 42]
+// Array-like object
+valuesTestObj = { 0: 'a', 1: 'b', 2: 'c' };
+console.log(Object.values(valuesTestObj), " == ", Object.myValues(valuesTestObj)); // ['a', 'b', 'c']
+// Array-like object with random key ordering
+valuesTestObj = { 100: 'a', 2: 'b', 7: 'c' };
+console.log(Object.values(valuesTestObj), " == ", Object.myValues(valuesTestObj)); // ['b', 'c', 'a']
+// getFoo is property which isn't enumerable
+valuesTestObj = Object.create({}, { getFoo: { value() { return this.foo; } } });
+valuesTestObj.foo = 'bar';
+console.log(Object.values(valuesTestObj), " == ", Object.myValues(valuesTestObj)); // ['bar']
+// non-object argument
+console.log(Object.values('foo'), " == ", Object.myValues('foo')); // ['f', 'o', 'o']
+
+
+*/
