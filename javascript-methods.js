b5b5cf491913532bc049855aff5d2698259ba3f3
mlavi16 <Maya.lavi11@gmail.com>
2022-10-07 12:58:46 -0400

    adding equals function for testing

--- javascript-methods.js
+++ javascript-methods.js
@@ -71,3 +71,45 @@ Object.myKeys = function(object) {
 Object.myValues = function(object) {
   // Place your code here.
 };
+
+
+
+
+// -----------------------Testing Functions--------------------------------- //
+
+Array.prototype.equals = function (array) {
+  // if the other array is a falsy value, return
+  if (!array)
+    return false;
+
+  // compare lengths - can save a lot of time 
+  if (this.length != array.length) {
+    console.log(this, " == ", array, " is ", false, " [0]");
+    return false;
+  }
+
+  for (var i = 0, l=this.length; i < l; i++) {
+    // Check if we have nested arrays
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      // recurse into the nested arrays
+      if (!this[i].equals(array[i])) {
+        console.log(this, " == ", array, " is ", false, " [1]");
+        return false;       
+      }
+    }           
+    else if (this[i] != array[i]) { 
+      // Warning - two different object instances will never be equal: {x:20} != {x:20}
+      console.log(this, " == ", array, " is ", false, " [2]");
+      return false;   
+    }           
+  } 
+  console.log(this, " == ", array, " is ", true);      
+  return true;
+}
+
+
+// Map //
+
+
+// Filter //
+
