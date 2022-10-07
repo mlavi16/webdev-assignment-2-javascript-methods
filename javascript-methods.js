46a81a40b5ac7873decaf3701e7132a3ca92f4f3
mlavi16 <Maya.lavi11@gmail.com>
2022-10-07 13:00:41 -0400

    testing filter func

--- javascript-methods.js
+++ javascript-methods.js
@@ -112,4 +112,19 @@ Array.prototype.equals = function (array) {
 
 
 // Filter //
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const result1 = words.filter(word => word.length > 6);
+const result2 = words.myFilter(word => word.length > 6);
+result1.equals(result2); // [ 'exuberant', 'destruction', 'present' ]
+
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+function isPrime(num) {
+  for (let i = 2; num > i; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+array.filter(isPrime).equals(array.myFilter(isPrime)) // [2, 3, 5, 7, 11, 13]
 
